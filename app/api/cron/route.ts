e4837505db4b1f2f093ab1938fceb4476ed43e13
Marian <marian.v.danci@gmail.com>
2023-10-05 23:53:50 +0300

    fix build errors 2 max duration fixed for free vercel plan which goes from 1 to 10 instead of 300 seconds

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -6,7 +6,7 @@ import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } f
 import { NextResponse } from "next/server";
 
 
-export const maxDuration = 300; // 5 minutes
+export const maxDuration = 10; // seconds
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
